a64b49f9a98b8eba2ffbe5a044accbcb6b66ee5a
Sebastien Valla <sv@massa.net>
2023-03-15 11:55:47 +0100

    first commit

---
+++ pkg/plugin/register.go
@@ -0,0 +1,103 @@
+package plugin
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+)
+
+const ThyraRegisterEndpoint = "http://my.massa/plugin-manager/register"
+
+type Info struct {
+	Name        string
+	Author      string
+	Description string
+	APISpec     string
+	Logo        string
+}
+
+type registerBody struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Description string `json:"description"`
+	Logo        string `json:"logo"`
+	URL         string `json:"url"`
+	APISpec     string `json:"api_spec"`
+	Home        string `json:"home"`
+}
+
+func RegisterPlugin(listener net.Listener, info Info) {
+	if os.Getenv("STANDALONE") == "1" {
+		return
+	}
+
+	minimumNumberOfCLIArgument := 2
+
+	if len(os.Args) >= minimumNumberOfCLIArgument {
+		err := register(os.Args[1], info, listener.Addr())
+		if err != nil {
+			log.Panicln(err)
+		}
+	} else {
+		panic("Usage: program must be started with a correlationID command line argument")
+	}
+}
+
+func register(
+	pluginID string,
+	info Info,
+	socket net.Addr,
+) error {
+	reg := registerBody{
+		ID:          pluginID,
+		Name:        info.Name,
+		Author:      info.Author,
+		Description: info.Description,
+		URL:         "http://" + socket.String(),
+		APISpec:     info.APISpec,
+		Logo:        info.Logo,
+		Home:        "",
+	}
+
+	body, err := json.Marshal(reg)
+	if err != nil {
+		return fmt.Errorf("while marshaling register body: %w", err)
+	}
+
+	request, err := http.NewRequestWithContext(context.Background(), http.MethodPost, ThyraRegisterEndpoint, bytes.NewBuffer(body))
+	if err != nil {
+		return fmt.Errorf("while creating register request: %w", err)
+	}
+
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return fmt.Errorf("while doing register request: %w", err)
+	}
+
+	if response.StatusCode != http.StatusNoContent {
+		body, _ := io.ReadAll(response.Body)
+		defer response.Body.Close()
+
+		var data interface{}
+
+		err = json.Unmarshal(body, &data)
+		if err != nil {
+			data = fmt.Errorf("unable to decode json response: %w", err)
+		}
+
+		return fmt.Errorf("plugin registry failed: HTTP status: %d, HTTP body: %v", response.StatusCode, data)
+	}
+
+	return nil
+}
---
+++ thyra-plugin-hello-world.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-openapi/loads"
+	"github.com/massalabs/thyra-plugin-hello-world/api"
+	"github.com/massalabs/thyra-plugin-hello-world/api/server/restapi"
+	"github.com/massalabs/thyra-plugin-hello-world/api/server/restapi/operations"
+	"github.com/massalabs/thyra-plugin-hello-world/pkg/plugin"
+	"github.com/massalabs/thyra-plugin-hello-world/web"
+)
+
+const logoFile = "logo_massa.webp"
+
+func killTime(quit chan bool) {
+
+	fmt.Fprintf(os.Stdout, "Plugin is initializing.\n")
+
+	for {
+		select {
+
+		case <-quit:
+			fmt.Fprintf(os.Stdout, "Plugin is shutting down.\nBye!\n")
+
+			return
+		}
+	}
+}
+
+func initializeAPI() *restapi.Server {
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		panic(err)
+	}
+
+	pluginAPI := operations.NewHelloWorldAPI(swaggerSpec)
+	server := restapi.NewServer(pluginAPI)
+
+	pluginAPI.WebHandler = operations.WebHandlerFunc(web.Handle)
+	pluginAPI.DefaultPageHandler = operations.DefaultPageHandlerFunc(web.DefaultRedirectHandler)
+	pluginAPI.HelloHandler = operations.HelloHandlerFunc(api.Hello)
+
+	server.ConfigureAPI()
+
+	return server
+}
+
+func main() {
+	quit := make(chan bool)
+	intSig := make(chan os.Signal, 1)
+	signal.Notify(intSig, syscall.SIGINT, syscall.SIGTERM)
+
+	go killTime(quit)
+
+	server := initializeAPI()
+
+	listener, err := server.HTTPListener()
+	if err != nil {
+		panic(err)
+	}
+
+	PluginAuthor := "Massalabs"
+	PluginName := "hello world"
+	PluginDescription := "A simple hello world plugin."
+
+	plugin.RegisterPlugin(listener, plugin.Info{
+		Name: PluginName, Author: PluginAuthor,
+		Description: PluginDescription, APISpec: "", Logo: logoFile,
+	})
+
+	if err := server.Serve(); err != nil {
+		panic(err)
+	}
+
+	<-intSig
+	quit <- true
+}
